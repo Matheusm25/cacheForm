42e2d2e922ab11fd2f54dd1a65b86c1fdf9b1ba8
Matheus Marques <matheusml25@hotmail.com>
2019-11-27 01:21:03 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+  const SIZE = localStorage.length;
+
+  const objs = [];
+  for (let i = 0; i < SIZE; i++) {
+    const name = localStorage.key(i);
+    const value = localStorage.getItem(name);
+    objs.push({
+      id: name,
+      value: value,
+    });
+  }
+
+  for (let i = 0; i < SIZE; i++) {
+    const e = document.querySelector('#' + objs[i].id);
+    if (e) {
+      e.value = objs[i].value;
+    }
+  }
+};
+
+/**
+ * Add cache for specified field
+ * @param {string} id id of the field that will be in cache
+ */
+function addCacheField(id) {
+  const e = document.querySelector('#' + id);
+
+  if (e) {
+    e.addEventListener('blur', function( event ) {
+      const value = e.value;
+      localStorage.setItem(id, value);
+    }, true);
+  }
+}
